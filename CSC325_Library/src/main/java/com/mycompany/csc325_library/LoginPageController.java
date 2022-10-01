0cc397a8224a059c556241b5abb007b6fdc1b300
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-01 12:26:02 -0400

    Created LoginPage controller, fxml and css files.

---
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/LoginPageController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.csc325_library;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author chriscanenguez
+ */
+public class LoginPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
