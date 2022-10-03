71d249270c6b159b2eca25e26ac685d7f3ece299
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-03 11:42:05 -0400

    Added login() method for LoginPageController in order to go from login page to main menu at runtime.

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/LoginPageController.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/LoginPageController.java
@@ -4,9 +4,15 @@
  */
 package com.mycompany.csc325_library;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +21,35 @@ import javafx.fxml.Initializable;
  */
 public class LoginPageController implements Initializable {
 
+    private Parent root;
+    
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) 
+    {
         // TODO
-    }    
+    } // End initialize.
     
-}
+    @FXML
+    public void login()
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainMenuPage.fxml"));
+        try 
+        {
+            root = fxmlLoader.load();
+        } catch (IOException ex) 
+        {
+            ex.printStackTrace();
+        }
+        // Gets the MainMenuController.
+
+        // Sets stage.
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    } // End login.   
+} // End LoginPageController.
