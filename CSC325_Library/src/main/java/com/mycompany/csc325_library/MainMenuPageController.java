ce7911ab3b724eaecfde93468e67051511a60631
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-04 17:50:41 -0400

    Added method that closes window when you press 'Logout' button.

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/MainMenuPageController.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/MainMenuPageController.java
@@ -7,11 +7,13 @@ package com.mycompany.csc325_library;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /**
@@ -23,9 +25,13 @@ public class MainMenuPageController implements Initializable {
 
     private Parent root1;
     private Parent root2;
+    
+    @FXML
+    public Button logoutButton;
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -33,26 +39,30 @@ public class MainMenuPageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     } // End initialize.
-    
+
     @FXML
-    public void goToCheckoutButton() throws IOException
-    {
+    public void goToCheckoutButton() throws IOException {
         FXMLLoader checkoutLoader = new FXMLLoader(getClass().getResource("BookCheckoutPage.fxml"));
         root1 = checkoutLoader.load();
-        
+
         Stage stage = new Stage();
         stage.setScene(new Scene(root1));
         stage.show();
     } // End goToCheckoutButton.
-    
+
     @FXML
-    public void goToAccountInfo() throws IOException
-    {
+    public void goToAccountInfo() throws IOException {
         FXMLLoader accountLoader = new FXMLLoader(getClass().getResource("AccountInfoPage.fxml"));
         root2 = accountLoader.load();
-        
+
         Stage stage = new Stage();
         stage.setScene(new Scene(root2));
         stage.show();
-    } // End goToCheckoutButton.
+    } // End goToAccountInfo.
+
+    @FXML
+    public void logoutButton(ActionEvent event) {
+        Stage stage = (Stage) logoutButton.getScene().getWindow();
+        stage.close();
+    } // End logoutButton.
 } // End MainMenuPageController.
