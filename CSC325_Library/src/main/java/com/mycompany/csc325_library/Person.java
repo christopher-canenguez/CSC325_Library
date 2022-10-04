1678338739fffb8a7a6ab9fd66e31fe135e7b4c0
kulsomzaraei <kulsomzaraei@localhost>
2022-10-04 14:42:40 -0400

    Person class, constructor, getters and setters, to string

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/Person.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/Person.java
@@ -6,9 +6,74 @@ package com.mycompany.csc325_library;
  */
 
 /**
- *
+ *Title: Person Class
+ * Description: Contains attributes of a person object 
  * @author kulsomzaraei
  */
 public class Person {
+    
+    /**
+     Instance Variables:
+     */
+    
+    private String firstName;
+    private String lastName;
+    private String personID;
+
+    /**
+     *Parameterized Constructor:
+     *stores the parameters into the appropriate instance variables
+     * @param firtName
+     * @param lastName
+     * @param personID
+     */
+    public void setPersonID(String personID) {
+        this.personID = personID;
+    }
+
+
+    public Person(String fName, String lName, String id) {
+        firstName = fName;
+        lastName = lName;
+        personID = id;
+    }
+    
+    
+        /**
+     *Getter and setters for person attributes
+     * @return
+     */
+    public String getFirstName(){
+        return firstName;
+    
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonID() {
+        return personID;
+    }
+    
+    
+    /**
+     *ToString method
+     */
+ 
+   @Override
+    public String toString() {
+        String personString = "Person{" + "firstName=" + firstName + ", lastName=" + lastName + ", personID=" + personID + '}';
+        return personString;
+    }
+    
     
 }
