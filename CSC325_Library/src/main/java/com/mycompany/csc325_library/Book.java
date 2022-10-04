e0ede547b2a97ebc364bc3a89737a79288d0a419
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-03 21:24:29 -0400

    Fixed module error and moved Book and Person classes to package with controllers.

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/Book.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/Book.java
@@ -1,3 +1,5 @@
+package com.mycompany.csc325_library;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +9,6 @@
  *
  * @author kulsomzaraei
  */
-public class Person {
+public class Book {
     
 }
