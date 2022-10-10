c7c574026e7535dd8cc6214a8f9f94e305e9ee6a
kulsomzaraei <kulsomzaraei@localhost>
2022-10-09 20:01:55 -0400

    Library class: Changed name from booklibrary to library and add book and user samples

---
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/Library.java
@@ -0,0 +1,46 @@
+package com.mycompany.csc325_library;
+
+
+import com.mycompany.csc325_library.Book;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author kulsomzaraei
+ */
+public class Library {
+   
+    
+    public void Library(){
+        
+       Person person1 = new Person(" "," "," "); 
+       Book[] bookList = new Book[10];
+       Book1("hi","hi","123","hi",person1);
+       Book Book1 = bookList[0];
+       Book Book2 = bookList[1];
+       Book Book3 = bookList[2];
+       Book Book4 = bookList[3];
+       Book Book5 = bookList[4];
+       Book Book6 = bookList[5];
+       Book Book7 = bookList[6];
+       Book Book8 = bookList[7];
+       Book Book9 = bookList[8];
+       Book Book10 = bookList[9];
+       
+       
+       
+       
+       
+       
+       
+       
+         
+        
+        
+    }
+    
+}
