77eebd1b50e0099760c970a57f333666d6837418
kulsomzaraei <kulsomzaraei@macbook-air>
2022-10-16 20:19:03 -0400

    Library class: Update/Create book, person, user samples with new attributes

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/Library.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/Library.java
@@ -18,21 +18,26 @@ public class Library {
     
     public void Library(){
        
+        //authors
        Person author1 = new Person("S.E", "Hinton");
+       Person author2 = new Person("J.K", "Rowling");
        
+
+      //users list 
        User[] userList = new User[4];
-       User user1 = userList[0];
+       User user1 = new User("Sam", "Hindy", 12);
+       user1 = userList[0];
        User user2 = userList[1];
        User user3 = userList[2];
        User user4 = userList[3];
        User user5 = userList[4];
  
+       //books list
        Book[] bookList = new Book[10];
-       
-       Book book1 = new Book("The Outsiders", author1, 3, "Not Available", user1);
-    
+       Book book1 = new Book("The Outsiders", author1, 123455, "Not Available", user1);
+       Book book2 = new Book("The Sorcerers Stone", author2, 434566, "Not Available", user2);
        book1 = bookList[0];
-       Book book2 = bookList[1];
+       book2 = bookList[1];
        Book book3 = bookList[2];
        Book book4 = bookList[3];
        Book book5 = bookList[4];
