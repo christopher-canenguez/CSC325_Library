6f8d78b055028d62b6215c57b4f04e8d1fa60dbe
kulsomzaraei <kulsomzaraei@macbook-air>
2022-10-26 21:01:16 -0400

    User class: add toString

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/User.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/User.java
@@ -8,15 +8,17 @@ package com.mycompany.csc325_library;
  *
  * @author user
  */
-public abstract class User extends Person {
-    private Book[] userCheckedOutBooks = new Book[4];
-    private String id;
+public class User extends Person {
+//    private Book[] userCheckedOutBooks = new Book[4];
+    public String id;
+   
     
-    public User(String fName, String lName) {
-    super(fName, lName);
-    this.userCheckedOutBooks = null;
+    public User(String name, int id) {
+    super(name);
+   // this.userCheckedOutBooks = null;
     this.id = "0";
     }
+
     
       public String getId() {
         return id;
@@ -26,13 +28,23 @@ public abstract class User extends Person {
         this.id = id;
     }
     
-    public Book[] getUserCheckedOutBooks() {
-        return userCheckedOutBooks;
+    public void setName(String name){
+    this.name = name;
     }
-
-    public void setUserCheckedOutBooks(Book[] userCheckedOutBooks) {
-        this.userCheckedOutBooks = userCheckedOutBooks;
-    }
-   
     
+    
+//    public Book[] getUserCheckedOutBooks() {
+  //      return userCheckedOutBooks;
+  //  }
+
+ //   public void setUserCheckedOutBooks(Book[] userCheckedOutBooks) {
+ //       this.userCheckedOutBooks = userCheckedOutBooks;
+ //  }
+   
+      @Override
+    public String toString() {
+        String myUser;
+        myUser = "Name: " + name + "ID:" + id;
+        return myUser;
+    }
 }
