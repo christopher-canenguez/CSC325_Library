baf20377cb3f1f38e8cbe08e1f0ed454fd59cc20
Christopher Canenguez <canenguez.chris@gmail.com>
2022-10-28 21:12:06 -0400

    Set up columns in the initialization method of databaseController.

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/DatabasePageController.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/DatabasePageController.java
@@ -8,6 +8,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -32,8 +33,20 @@ public class DatabasePageController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // Set up the columns of the table.
+        isbnColumn.setCellValueFactory(new PropertyValueFactory<Book, Integer>("isbn"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("titleColumn"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("authorColumn"));
+        availabilityColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("availabilityColumn"));
+
+        // Load Books.
+        tableView.setItems(getBooks());
     } // End initialize.
+    
+    public ObservableList<Book> getBooks()
+    {
+        
+    }
     
     @FXML
     public void exitButtonEvent(ActionEvent event) {
