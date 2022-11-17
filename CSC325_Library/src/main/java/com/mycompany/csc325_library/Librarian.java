bfe65d9466f946418cb04e41134525cc779a8598
kulsomzaraei <kulsomzaraei@macbook-air>
2022-11-17 16:12:19 -0500

    add librarian person

---
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/Librarian.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_library;
+
+/**
+ *
+ * @author kulsomzaraei
+ */
+public class Librarian extends Person {
+
+    public String librarianID;
+    public int pinCode;
+
+    public Librarian(String id, int pin) {
+        super(name);
+        // this.userCheckedOutBooks = null;
+        this.id = id;
+        this.pinCode = pin;
+
+    } // End User.
+
+}
