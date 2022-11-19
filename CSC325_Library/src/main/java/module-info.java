844a70c6f529d511c4dcf58fdd9fe6cd93c8d331
shameedjob <shameed.job.32@gmail.com>
2022-11-19 17:08:39 -0500

    Fixed pom and module info

--- CSC325_Library/src/main/java/module-info.java
+++ CSC325_Library/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.mycompany.csc325_library {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
+    requires com.google.gson;
 
     opens com.mycompany.csc325_library to javafx.fxml;
     exports com.mycompany.csc325_library;
