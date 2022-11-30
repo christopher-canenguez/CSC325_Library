af8e030410b22bf4d4eaf9113d1f088ae495e89a
Christopher Canenguez <canenguez.chris@gmail.com>
2022-11-30 11:40:27 -0500

    Added showInfo button in account info page, where it will hide user info at first until the button is pressed, and hidden once again when the button is presed.

--- CSC325_Library/src/main/java/com/mycompany/csc325_library/AccountInfoPageController.java
+++ CSC325_Library/src/main/java/com/mycompany/csc325_library/AccountInfoPageController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -27,6 +28,13 @@ public class AccountInfoPageController implements Initializable {
     @FXML
     TextField accountPinTextField;
 
+    @FXML
+    PasswordField accountNamePasswordField;
+    @FXML
+    PasswordField accountNumberPasswordField;
+    @FXML
+    PasswordField accountPinPasswordField;
+
     /**
      * Initializes the controller class.
      *
@@ -35,10 +43,7 @@ public class AccountInfoPageController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        accountNameTextField.setText(LoginPageController.currentUser.getName());
-        accountNumberTextField.setText(String.valueOf(LoginPageController.currentUser.getId()));
-        accountPinTextField.setText(String.valueOf(LoginPageController.currentUser.getPinCode()));
-
+        setPasswordAndTextFields();
     } // End initialize.
 
     @FXML
@@ -47,4 +52,51 @@ public class AccountInfoPageController implements Initializable {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
     } // End exitButtonEvent.
+
+    private void setPasswordAndTextFields() {
+        accountNamePasswordField.setText(LoginPageController.currentUser.getName());
+        accountNumberPasswordField.setText(String.valueOf(LoginPageController.currentUser.getId()));
+        accountPinPasswordField.setText(String.valueOf(LoginPageController.currentUser.getPinCode()));
+
+        accountNameTextField.setText(LoginPageController.currentUser.getName());
+        accountNumberTextField.setText(String.valueOf(LoginPageController.currentUser.getId()));
+        accountPinTextField.setText(String.valueOf(LoginPageController.currentUser.getPinCode()));
+    }
+
+    @FXML
+    public void showInfoEvent(ActionEvent event) {
+        if (isInfoVisible()) {
+            hideInfo();
+        } else {
+            showInfo();
+        }
+
+    }
+
+    private boolean isInfoVisible() {
+        if (accountNameTextField.isVisible()) {
+            return true;
+        }
+        return false;
+    }
+
+    private void showInfo() {
+        accountNamePasswordField.setVisible(false);
+        accountNumberPasswordField.setVisible(false);
+        accountPinPasswordField.setVisible(false);
+
+        accountNameTextField.setVisible(true);
+        accountNumberTextField.setVisible(true);
+        accountPinTextField.setVisible(true);
+    }
+
+    private void hideInfo() {
+        accountNamePasswordField.setVisible(true);
+        accountNumberPasswordField.setVisible(true);
+        accountPinPasswordField.setVisible(true);
+
+        accountNameTextField.setVisible(false);
+        accountNumberTextField.setVisible(false);
+        accountPinTextField.setVisible(false);
+    }
 } // End AccountInfoPageController.
